821575e47fbe41fe36f533bd3767fc5859891c66
c2HsZrIg6pCyioh4 <c2HsZrIg6pCyioh4>
2025-09-03 17:34:26 +0800

    no message

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -2,7 +2,7 @@
   <footer  v-if="!isReaderPage" class="footer">
     <FriendLinks />
     <p >
-      本站所有小说内容均由网友上传分享，仅供学习与交流使用，版权归原作者所有。如有侵权，请联系<a :href="`mailto:contact@${domain}`">contact@{{ domain }}</a>删除<br>
+      本站所有小说内容均由网友上传分享，仅供学习与交流使用，版权归原作者所有。如有侵权，请联系<a :href="`mailto:contact@${rootDomain}`">contact@{{ rootDomain }}</a>删除<br>
     </p>
     <p>© 2025 Novel Reader · All rights reserved.</p>
   </footer>
@@ -14,8 +14,31 @@ import { useRoute } from 'vue-router'
 import FriendLinks from "./FriendLinks.vue";
 const route = useRoute()
 const isReaderPage = computed(() => route.path.startsWith('/reader'))
+function getRootDomain(hostname: string): string {
+  // 处理本地开发环境
+  if (
+      hostname === 'localhost' ||
+      hostname === '127.0.0.1' ||
+      hostname === '::1'
+  ) {
+    return hostname
+  }
+
+  const parts = hostname.split('.')
+  if (parts.length <= 2) return hostname
+
+  // 处理常见的双后缀情况
+  const tlds = ['com.cn', 'net.cn', 'org.cn', 'gov.cn', 'co.uk']
+  const lastTwo = parts.slice(-2).join('.')
+  const lastThree = parts.slice(-3).join('.')
+  if (tlds.includes(lastTwo)) {
+    return lastThree
+  }
+  return lastTwo
+}
 // 动态获取当前域名
-const domain = window.location.hostname
+// 动态获取当前域名
+const rootDomain = getRootDomain(window.location.hostname)
 </script>
 
 <style scoped>
